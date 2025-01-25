cdc4ce3deaf5bf4cfcbd3e29fb964446a169a3e0
bhuescas <bhuescas34@gmail.com>
2025-01-25 17:40:30 -0600

    Revert "Progress of traffic code"
    This reverts commit fb62d48ef86f4dccc57bc067e51a1fb90374d271.

--- simulation.py
+++ simulation.py
@@ -20,6 +20,7 @@ def calculate_scaling_percentages(screen_width, screen_height):
     height_adjustment_percent = screen_height / original_height
     return width_adjustment_percent, height_adjustment_percent
 
+
 def initialize_pygame():
     pygame.init()
     screen_width, screen_height = get_screen_size()
@@ -29,7 +30,6 @@ def initialize_pygame():
 
 def load_images():
     images = {
-        #Vechicles#
         "car_north": pygame.image.load("images/car_northside.png"),
         "car_south": pygame.image.load("images/car_southside.png"),
         "car_east": pygame.image.load("images/car_eastside.png"),
@@ -42,14 +42,6 @@ def load_images():
         "truck_south": pygame.image.load("images/truck_southside.png"),
         "truck_east": pygame.image.load("images/truck_eastside.png"),
         "truck_west": pygame.image.load("images/truck_westside.png"),
-        
-        #Streetlights#
-        "streetlight_red_hor": pygame.image.load(""),
-        "streetlight_yellow_hor": pygame.image.load(""),
-        "streetlight_green_horni": pygame.image.load(""),
-        "streetlight_red_vertical": pygame.image.load(""),
-        "streetlight_yellow_vertical": pygame.image.load(""),
-        "streetlight_green_vertical": pygame.image.load(""),
 
         "background": pygame.image.load("images/Grid_background.png")
     }
@@ -103,7 +95,6 @@ def generate_cars(screen_width, screen_height):
         ]
     }
 
-<<<<<<< Updated upstream
     cars = []
     for direction, points in car_positions.items():
         for static, ml in points:
@@ -111,81 +102,7 @@ def generate_cars(screen_width, screen_height):
             cars.append(Car(ml[0], ml[1], direction, width_adjustment_percent, height_adjustment_percent))
 
     return cars
-=======
-    while True:  # Continuously generate cars
-        direction = random.choice(list(car_positions.keys()))
-        static, ml = random.choice(car_positions[direction])
 
-        cars.append(Car(static[0], static[1], direction, width_adjustment_percent, height_adjustment_percent))
-        cars.append(Car(ml[0], ml[1], direction, width_adjustment_percent, height_adjustment_percent))
-
-        time.sleep(1)
-
-
-
-    # cars = []
-    # for _ in range(100):  # Run for 100 seconds
-    #     direction = random.choice(list(car_positions.keys()))
-    #     static, ml = random.choice(car_positions[direction])
-
-    #     cars.append(Car(static[0], static[1], direction, width_adjustment_percent, height_adjustment_percent))
-    #     cars.append(Car(ml[0], ml[1], direction, width_adjustment_percent, height_adjustment_percent))
-
-    #     time.sleep(1)
-
-
-    # direction = random.choice(list(car_positions.keys()))
-    # static , ml = random.choice(car_positions[direction])
-    # cars.append(Car(static[0], static[1], direction, width_adjustment_percent, height_adjustment_percent))
-    # cars.append(Car(ml[0], ml[1], direction, width_adjustment_percent, height_adjustment_percent))
-    # time.sleep(0.5)
-
-    #return cars
-
-    # for direction, points in car_positions.items():
-    #     for static, ml in points:
-    #         cars.append(Car(static[0], static[1], direction, width_adjustment_percent, height_adjustment_percent))
-    #         cars.append(Car(ml[0], ml[1], direction, width_adjustment_percent, height_adjustment_percent))
-
-def generate_streetlights(screen, screen_width, screen_height):
-    width_adjustment_percent, height_adjustment_percent = calculate_scaling_percentages(screen_width, screen_height)
-    streetlight_positions = {
-        "horizontal": [
-
-            [(int(389 * width_adjustment_percent), int(1600 * height_adjustment_percent))],
-            [(int(430 * width_adjustment_percent), int(1600 * height_adjustment_percent))],
-            [(int(809 * width_adjustment_percent), int(1600 * height_adjustment_percent))],
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-        ]
-        "vertical": [
-            
-            [(int(389 * width_adjustment_percent), int(1600 * height_adjustment_percent))],
-            [(int(430 * width_adjustment_percent), int(1600 * height_adjustment_percent))],
-            [(int(809 * width_adjustment_percent), int(1600 * height_adjustment_percent))],
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-            [(int(851 * width_adjustment_percent), int(1600 * height_adjustment_percent))]
-        ]
-    }
->>>>>>> Stashed changes
-
-    streetlights = []
-    streetlight_images = {
-        "red": images["streetlight_red"],
-        "yellow": images["streetlight_yellow"],
-        "green": images["streetlight_green"]
-    }
-
-    for position in positions:
-        x, y, orientation = position
-        streetlights.append(Streetlight(x, y, orientation, screen, streetlight_images))
-    return streetlights
 
 def process_background(background, screen_size):
     rotated_background = pygame.transform.rotate(background, 90)
@@ -210,66 +127,6 @@ class Car:
     def render(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
-<<<<<<< Updated upstream
-=======
-class streetlight:
-    def __init__(self, x, y, orientation, screen, images);
-        self.x = x
-        self.y = y
-        self.orientation = orientation
-        self.screen = screen
-        self.images = images
-        self.state = "red"  # Start with red light
-        self.timer = 0  # Timer to track switching
-    def switch_state(self)
-        """Switch the streetlight state based on the timer."""
-        if self.state == "red":
-            self.state = "green"
-        elif self.state == "green":
-            self.state = "yellow"
-        elif self.state == "yellow":
-            self.state = "red"
-    def render(self):
-        """Draw the current light on the screen."""
-        self.screen.blit(self.images[self.state], (self.x, self.y))
-
-def update_streetlights(streetlights):
-     """
-    Continuously update the state of all streetlights.
-    :param streetlights: List of Streetlight objects.
-    """
-    while True:
-        for light in streetlights:
-            light.switch_state()  # Switch to the next state
-        time.sleep(5)  # Wait 5 seconds before switching again
-
-def vehicle_vanishing(vehicle,x,y, screen_width, screen_height):
-    width_adjustment_percent, height_adjustment_percent = calculate_scaling_percentages(screen_width, screen_height)
-    vanishing_points = [
-        (int(389 * width_adjustment_percent), int(4 * height_adjustment_percent)) , (int(809 * width_adjustment_percent), int(4 * height_adjustment_percent)) ,
-        (int(1570 * width_adjustment_percent), int(4 * height_adjustment_percent)) , (int(1990 * width_adjustment_percent), int(4 * height_adjustment_percent)) ,
-        (int(430 * width_adjustment_percent), int(4 * height_adjustment_percent)) , (int(851 * width_adjustment_percent), int(4 * height_adjustment_percent)) ,
-        (int(1608 * width_adjustment_percent), int(4 * height_adjustment_percent)) , (int(2032 * width_adjustment_percent), int(4 * height_adjustment_percent)) ,
-        (int(309 * width_adjustment_percent), int(1600 * height_adjustment_percent)) , (int(730 * width_adjustment_percent), int(1600 * height_adjustment_percent)) ,
-        (int(1490 * width_adjustment_percent), int(1600 * height_adjustment_percent)) , (int(1910 * width_adjustment_percent), int(1600 * height_adjustment_percent)) ,
-        (int(350 * width_adjustment_percent), int(1600 * height_adjustment_percent)) , (int(770 * width_adjustment_percent), int(1600 * height_adjustment_percent)) ,
-        (int(1530 * width_adjustment_percent), int(1600 * height_adjustment_percent)) , (int(1953 * width_adjustment_percent), int(1600 * height_adjustment_percent)) ,
-        (int(1127 * width_adjustment_percent), int(552 * height_adjustment_percent)) , (int(1127 * width_adjustment_percent), int(1092 * height_adjustment_percent)) ,
-        (int(2317 * width_adjustment_percent), int(552 * height_adjustment_percent)) , (int(2317 * width_adjustment_percent), int(1092 * height_adjustment_percent)) ,
-        (int(1127 * width_adjustment_percent), int(591 * height_adjustment_percent)) , (int(1127 * width_adjustment_percent), int(1131 * height_adjustment_percent)) ,
-        (int(2317 * width_adjustment_percent), int(591 * height_adjustment_percent)) , (int(2317 * width_adjustment_percent), int(1131 * height_adjustment_percent)) ,
-        (int(4 * width_adjustment_percent), int(472 * height_adjustment_percent)) , (int(4 * width_adjustment_percent), int(1011 * height_adjustment_percent)) ,
-        (int(1191 * width_adjustment_percent), int(472 * height_adjustment_percent)) , (int(1191 * width_adjustment_percent), int(1011 * height_adjustment_percent)) ,
-        (int(4 * width_adjustment_percent), int(510 * height_adjustment_percent)) , (int(4 * width_adjustment_percent), int(1050 * height_adjustment_percent)) ,
-        (int(1191 * width_adjustment_percent), int(510 * height_adjustment_percent)) , (int(1191 * width_adjustment_percent), int(1050 * height_adjustment_percent))
-    ]
-
-    for vx , vy in vanishing_points:
-        if abs(x-vx) <=1 and abs (y - vy) <= 1:
-            return True
-    return False
-
->>>>>>> Stashed changes
 def main():
     screen = initialize_pygame()
     images = load_images()
@@ -281,12 +138,6 @@ def main():
 
     cars = generate_cars(screen_width, screen_height)
 
-    streetlight = []
-    streetlight_thread = Thread(target=update_streetlights, args=(streetlights, screen_width, screen_height))
-    streetlight_thread.daemon = True
-    streetlight_thread.start()
-
-    clock = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
@@ -297,9 +148,6 @@ def main():
 
         for car in cars:
             car.render(screen)
-
-        for light in streetlights:
-            light.render()
 
         pygame.display.flip()
         time.sleep(0.02)
